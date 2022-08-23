ca9f44e0262b861db0081fadac74f0c5612dddff
Jackeline Lima <jackelinelimap@gmail.com>
2022-08-23 14:43:53 -0300

    create file preferences view controller

---
+++ Mart/Mart/PreferencesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PreferencesViewController.swift
+//  Mart
+//
+//  Created by Jackeline Pires De Lima on 23/08/22.
+//
+
+import UIKit
+
+class PreferencesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
